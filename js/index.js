3146bf13eaf22a5ace39aec6f707517afb053418
AroniasPrenovost <Aroniascoder@gmail.com>
2017-01-22 18:06:45 -0800

    wireframed features - just need to add styling specific to the shop

--- js/index.js
+++ js/index.js
@@ -2,18 +2,16 @@
 
 
 var quotesArr = [];
-quotesArr[0] = '"Playfully doing something difficult, whether useful or not, that is hacking." <br><br> - Zed Shaw | Everett, WA';
-quotesArr[1] = '"Sharing is good, and with digital technology, sharing is easy." <br><br> - Zed Shaw | Everett, WA';
-quotesArr[2] = '"Most good programmers do programming...because it is fun to program." <br><br> Linus Torvalds';
-quotesArr[3] = '"Talk is cheap. Show me the code." <br><br> - Zed Shaw | Everett, WA';
-quotesArr[4] = '"There are only two kinds of languages: the ones people complain about and the ones nobody uses." <br><br> Bjarne Stroustrup';
-quotesArr[5] = '"I think it&apos;s a new feature. Don&apos;t tell anyone it was an accident." <br><br> Larry Wall';
-quotesArr[6] = '“Obsolete comments are worse than no comments.” <br><br> - Zed Shaw | Everett, WA';
-quotesArr[7] = '"One of my most productive days was throwing away 1,000 lines of code." <br><br> - Zed Shaw | Everett, WA';
-quotesArr[8] = '"Design and programming are human activities; forget that and all is lost." <br><br> - Zed Shaw | Everett, WA';
-quotesArr[9] = '"Just take it slow and do not hurt your brain." <br><br> - Zed Shaw | Everett, WA';
-quotesArr[10] = '"I realized that the feeling of making progress is just as important as making actual progress." <br><br> - Zed Shaw | Everett, WA';
-quotesArr[11] = '"Clearly, I reject the view that there is only one way that is right for everyone and for every problem." <br><br> Bjarne Stroustrup';
+quotesArr[0] = '<span style="font-style:italic" "font-size:20px">"I asked Chris if he could make some adjustments to my hair after finishing and he was happy to oblige.  He truly takes his time to make sure everything is perfect. I even got a neck/head massage at the end! I will definitely get my haircut from him again. Fantastic barber!</span>" <br><br> - Mark Guzman | Seattle, WA';
+quotesArr[1] = '<span style="font-style:italic" "font-size:20px">"After ten years of cutting my own hair, I decided to get it cut for this first time recently. I found this little shop after reading so many reviews of people following this particular barber, Chris Nguyen from his old establishment. I\'m so glad I came to this barbershop, and loved the fact that they were playing Sportcenter on TV.</span>" <br><br> - Zed Shaw | Everett, WA';
+quotesArr[2] = '<span style="font-style:italic" "font-size:20px">"I got a fantastic haircut. I have very thick difficult hair that stands nearly straight out, and needs to be "sculpted" more than trimmed lol. Chris did an excellent job, expertly translated some of my clumsy instructions into what I was looking for. Biggest thing I noticed is attention to detail - clean lines, immaculate symmetry at all the edges, and really nice work at the temples and behind the ears. Chris could easily charge twice as much for a haircut here (certainly all the others shops do!), but he barely charges beer money for a great service. I will be back!</span>" <br><br> Cameron Stahl | Seattle, WA';
+quotesArr[3] = '<span style="font-style:italic" "font-size:20px">"I was skeptical at first with the small number of reviews, but Chris was amazing. Parking is tight, but apparently the church across the street doesn\'t mind if you park there. I had only a vague idea of the haircut I wanted, and Chris was able to fill in the rest. The service provided was significantly better than Supercuts, my old go-to, and CHEAPER. I left him a fifty percent tip, because I felt the price didn\'t match the quality. Chris, you need to charge more!</span>" <br><br> - Garret S. | Greenlake, WA';
+quotesArr[4] = '<span style="font-style:italic" "font-size:20px">"Quick, friendly service with super low prices. A cut I would have gotten at another place for my long hair cost $15 less at Unique Barber (I paid $20 instead of $35). I was helped by Chris Nyugen. He also has a manicure license but doesn\'t have a wide selection of nail polish yet (Chris hasn\'t been at this particular location very long). I might be coming in for more than a haircut in the future!</span>" <br><br> Kristin J. | Seattle, WA';
+quotesArr[5] = '<span style="font-style:italic" "font-size:20px">"Guy named Chris great guy. This little guy is genuinely happy and great at his job. I\'d recommend friends, family and whoever to stop by to get a hair cut b! this guy. Can\'t wait to go see him next week perfect visit every time.</span>" <br><br> Jordan H. | Seattle, WA';
+quotesArr[6] = '<span style="font-style:italic" "font-size:20px">"Obsolete comments are worse than no comments.</span>" <br><br> - Zed Shaw | Everett, WA';
+quotesArr[7] = '<span style="font-style:italic" "font-size:20px">"One of my most productive days was throwing away 1,000 lines of code.</span>" <br><br> - Zed Shaw | Everett, WA';
+quotesArr[8] = '<span style="font-style:italic" "font-size:20px">"Design and programming are human activities; forget that and all is lost.</span>" <br><br> - Zed Shaw | Everett, WA';
+
 
 // cycle through the quotes
 setInterval(displayDetails, 3500);
@@ -22,6 +20,8 @@ function displayDetails(){
 	var randomQuote = Math.floor(Math.random() * (quotesArr.length - 0));
 	document.getElementById("quote").innerHTML = quotesArr[randomQuote];
 	};
+
+
 
 
 
